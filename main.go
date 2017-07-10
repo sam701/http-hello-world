621af53a5d1f7765d0210842e36f3ae52ed32a06
Alexei Samokvalov <alexei@sam701.com>
2017-07-10 16:40:22 +0200

    Add launch ID

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,9 +12,12 @@ import (
 
 var (
 	serverId = createId(10)
+	launchId = flag.String("launchId", "none", "Launch ID")
 )
 
 func main() {
+	flag.Parse()
+
 	http.HandleFunc("/", serve)
 	log.Println("Starting service on port 9000")
 	http.ListenAndServe(":9000", nil)
@@ -22,6 +26,7 @@ func main() {
 func serve(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "ServerId:", serverId)
 	fmt.Fprintln(w, "Timestamp:", time.Now().Format(time.RFC3339))
+	fmt.Fprintln(w, "LaunchID:", *launchId)
 }
 
 func createId(length int) string {
